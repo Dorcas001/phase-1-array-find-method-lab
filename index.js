ec6bbe2a43cb35d79e38f3b2a02818b1f26af602
karimi <karimidorcas1@gmail.com>
2023-12-28 14:05:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+function superbowlWin(record){
+    let result1 = record.find((x) => x.result === 'W');
+    if(result1){
+        return result1.year;
+    }else{
+        return undefined
+    }
+}
+
